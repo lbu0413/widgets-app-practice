0bc683762e2295d7637e13b754f77ffa11f597f1
LBU <lbu@LBUui-MacBook-Pro.local>
2020-10-26 15:46:35 -0400

    more commits

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -20,20 +20,34 @@ const Search = () => {
             
             setResults(data.query.search)
         }
+        const timeoutId = setTimeout(() => {
+            if(term){
+                search()
+            }
+        }, 500)
+        return () => {
+            clearTimeout(timeoutId)
+        }
         
-        search() 
     }, [term])
 
     const renderedResults = results.map((result) => {
         return (
             <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a 
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
+                </div>
                 <div className="content">
                     <div className="header">
                         {result.title}
                     </div>
                     <span dangerouslySetInnerHTML={{ __html: result.snippet }}>
                     </span>
-                    {result.snippet}
                 </div>
             </div>
         )
